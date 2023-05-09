43f7f71f8806462541ac12e0133d943b3577bc01
HamzaHassanIrshad <hamzahassanoff@gmail.com>
2023-05-09 10:42:14 +0530

    image upload part done

--- frontend/src/components/food-selector/index.js
+++ frontend/src/components/food-selector/index.js
@@ -1,8 +1,32 @@
 import React, { useState } from "react";
 import { Select } from "antd";
+import axios from "axios";
+import { useDropzone } from "react-dropzone";
 
 const FoodSelector = ({ foods, onChange }) => {
   const [selectedFood, setSelectedFood] = useState();
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleUpload = async () => {
+    if (!selectedFile) {
+      console.log("No file selected");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+
+    try {
+      await axios.post("/", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log("Image uploaded successfully");
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    }
+  };
 
   const handleChange = (value) => {
     onChange(value);
@@ -11,6 +35,24 @@ const FoodSelector = ({ foods, onChange }) => {
 
   const filterOption = (input, option) =>
     option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: (acceptedFiles) => {
+      const file = acceptedFiles[0];
+      if (file) {
+        const fileTypes = ["image/jpeg", "image/jpg", "image/png"];
+        const fileType = file.type;
+        if (fileTypes.includes(fileType)) {
+          setSelectedFile(file);
+        } else {
+          console.log(
+            "Invalid file type. Please select a JPG, JPEG, or PNG file."
+          );
+        }
+      }
+    },
+    multiple: false, // Limit to one image at a time
+  });
 
   return (
     <div className="foodSelector">
@@ -32,6 +74,29 @@ const FoodSelector = ({ foods, onChange }) => {
           </Select.Option>
         ))}
       </Select>
+      <div {...getRootProps()} className="upload-container">
+        <input {...getInputProps()} />
+        {selectedFile ? (
+          <div
+            style={{
+              width: "200px",
+              height: "200px",
+              marginBottom: "10px",
+              overflow: "hidden",
+            }}
+          >
+            <img
+              src={URL.createObjectURL(selectedFile)}
+              alt="Uploaded"
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+          </div>
+        ) : (
+          <p>Drag and drop an image here, or click to select a file</p>
+        )}
+        {selectedFile && <p>{selectedFile.name}</p>}
+        <button onClick={handleUpload}>Upload Image</button>
+      </div>
     </div>
   );
 };
